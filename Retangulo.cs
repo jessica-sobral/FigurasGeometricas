8e5f1075c9cd3e766e8da9429209e0545772818d
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2022-04-21 22:02:40 -0300

    modificação no cálculo do perímetro do retângulo

--- Retangulo.cs
+++ Retangulo.cs
@@ -3,7 +3,7 @@ public class Retangulo
     public double Base { get; set; }
     public double Altura { get; set; }
     public double Area { get => Base * Altura;}
-    public double Perimetro { get => 2* (Base + Altura); }
+    public double Perimetro { get => (Base * 2) + (Altura * 2); }
     public Retangulo(double _base, double altura)
     {
         Base = _base;
